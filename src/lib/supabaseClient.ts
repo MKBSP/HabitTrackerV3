afc536ac70c48fe2563329dfb009b64d94120103
Mads Schmidt Petersen <MadsTrundte@MacBook-Pro-7.local>
2025-01-27 16:54:05 +0000

    moving env info vercel

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,4 +1,3 @@
-
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.PUBLIC_SUPABASE_URL;
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,5 @@
 import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 export default defineConfig({
 	plugins: [sveltekit()],
